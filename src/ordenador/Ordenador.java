8b0813fbab573e278f2675f5bc6012e22cced9ec
Jose Alvarez Barciela <jalvarezbarciela@nautilus6.danielcastelao.org>
2019-10-22 10:40:12 +0200

    ordenador corregido

--- src/ordenador/Ordenador.java
+++ src/ordenador/Ordenador.java
@@ -4,27 +4,36 @@ package ordenador;
  * @author jalvarezbarciela
  */
 public class Ordenador {
-    private Rato Rato=new Rato();
-    private Pantalla Pantalla=new Pantalla();
+    private Rato rato=new Rato();
+    private Pantalla pantalla=new Pantalla();
     private CPU CPU=new CPU();
     public Ordenador(String tipo,String marca,float pulgada,int velocidade,int memoria){
-        Rato=new Rato(tipo);
-        Pantalla=new Pantalla(marca,pulgada);
+        rato=new Rato(tipo);
+        pantalla=new Pantalla(marca,pulgada);
         CPU=new CPU(velocidade,memoria);
     }
     public Ordenador(){
         
     }
     public void setRato(String tipo){
-        Rato=new Rato(tipo);
+        rato=new Rato(tipo);
     }
     public void setPantalla(String marca,float pulgadas){
-        Pantalla=new Pantalla(marca,pulgadas);
+        pantalla=new Pantalla(marca,pulgadas);
     }
     public void setCPU(int velocidade,int memoria){
         CPU=new CPU(velocidade,memoria);
     }
+    public Rato getRato(){
+        return rato;
+    }
+    public Pantalla getPantalla(){
+        return pantalla;
+    }
+    public CPU getCPU(){
+        return CPU;
+    }
     public void amosar(){
-        System.out.println("Rato:"+Rato.getTipo()+"\nPantalla: \nMarca:"+Pantalla.getMarca()+"\nPulgadas:"+Pantalla.getPulgadas()+"\nCPU:\nVelocidade:"+CPU.getVelocidade()+"\nMemoria:"+CPU.getMemoria());
+        System.out.println("Rato:"+rato.getTipo()+"\nPantalla: \nMarca:"+pantalla.getMarca()+"\nPulgadas:"+pantalla.getPulgadas()+"\nCPU:\nVelocidade:"+CPU.getVelocidade()+"\nMemoria:"+CPU.getMemoria());
     }
 }
